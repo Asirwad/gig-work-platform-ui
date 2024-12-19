5a31b841e421efca0943ced37c0a99a52940715d
Asirwad <asirwadsali@gmail.com>
2024-12-19 11:15:24 +0530

    init app config file and mapped port init with craco config

--- craco.config.js
+++ craco.config.js
@@ -1,9 +1,13 @@
 const path = require("path");
+const appConfig = require("./src/AppConfig.json");
 
 module.exports = {
   webpack: {
     alias: {
       "@": path.resolve(__dirname, "src"),
     },
+  },
+  devServer: {
+    port: appConfig.port,
   },
 };

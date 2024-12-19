0686301a7df91cd536d3ddf3f65b40391f7cf065
Asirwad <asirwadsali@gmail.com>
2024-12-19 11:43:47 +0530

    binded currentUserRole with App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,10 @@ import React, { useState } from "react";
 import Workers from "./components/Workers";
 import "./index.css";
 import { Creator } from "./components/Creator";
+import appConfig from "./AppConfig.json"
 
 export default function App() {
-  const [userRole, setUserRole] = useState("creator");
+  const [userRole, setUserRole] = useState(appConfig.currentUserRole);
 
   return (
     <div className="app-container">

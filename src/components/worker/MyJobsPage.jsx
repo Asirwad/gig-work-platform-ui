96f5936b97bd31be52c22edc9da1dd17eaa9f213
Asirwad <asirwadsali@gmail.com>
2025-01-23 16:46:33 +0530

    filter out revoked gigs, rem unused code

--- src/components/worker/MyJobsPage.jsx
+++ src/components/worker/MyJobsPage.jsx
@@ -41,6 +41,10 @@ export function MyJobsPage({ onViewGig, onNavigate }) {
     fetchInterestedGigs();
   }, []);
 
+  useEffect(()=>{
+    setinterestedGigs(interestedGigs.filter((gig) => gig.status !== 'revoked'));
+  }, [interestedGigs])
+
 
   return (
     <div className="min-h-screen bg-gray-100">
@@ -80,18 +84,12 @@ export function MyJobsPage({ onViewGig, onNavigate }) {
 
         ) : (
           <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
-            {interestedGigs.map((gig) => (
+            {interestedGigs.length>0 && interestedGigs.map((gig) => (
               <Card key={gig._id} className="bg-white flex flex-col h-full">
                 <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
                   <CardTitle badge={gig.gig_engagement_status} className="text-xl font-bold">
                     {gig.topic}
                   </CardTitle>
-                  {/* <Tooltip title={gig.ustar_category} arrow>
-                    <Button variant="ghost" size="icon" className="text-yellow-500">
-                      <Star className="h-4 w-4" />
-                      <span className="ml-1">{getUStarPoint.get(gig.ustar_category)}</span>
-                    </Button>
-                  </Tooltip> */}
                   <Button variant="ghost" size="icon" className="text-yellow-500">
                       <Star className="h-4 w-4" />
                       <span className="ml-1">{getUStarPoint.get(gig.ustar_category)}</span>
